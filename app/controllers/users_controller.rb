1815202e7f057ef9400ed1c642d9092c0fb56073
ITaravana <iamthebou@gmail.com>
2016-04-25 15:09:11 +0800

    tamere

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,13 @@
-require "byebug"
-# in users_controller.rb
+enable :sessions
+
 post '/signup' do
 
-
-
-
   user = User.new(params[:user])
+  user.password = params[:encrypted_password]
 
-  if user.save
-    # what should happen if the user is save?
+  if user.valid?
+	user.save
     redirect "/user_profile"
-
   else
     # what should happen if the user keyed in invalid date?
 
@@ -19,8 +16,30 @@ post '/signup' do
 end 
 
 
-get "/user_profile"
+get "/user_profile" do
 
 erb :"static/user_profile"
+
+end 
+
+get "/login" do
+
+erb :"static/login"
+
+end 
+
+get "/login_user" do
+
+# apply a authentication method to check if a user has entered a valid email and password
+  # if a user has successfully been authenticated, you can assign the current user id to a session
+
+end
+
+end 
+
+get "/logout_user" do
+
+  # kill a session when a user choses to logout, for example assign nil to a session
+  # redirect to the appropriate page
 
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,36 @@
 require 'bcrypt'
 
-
-# validates_uniqueness
-# validates
-# validates
-
+# attr_accessor :password
 
 class User < ActiveRecord::Base
+include BCrypt
+	validates :username, presence: true, length: { minimum: 4 }
+	validates :firstname, presence: true, length: { minimum: 4 }
+	validates :lastname, presence: true, length: { minimum: 4 }
+	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/,
+	    message: "Please enter a valid email!" }
+	validates :encrypted_password, presence: true, length: { in: 4..60 }
+
+# end
+
+
+# class Password < ActiveRecord::Base
+
+	 def password
+	    @password ||= Password.new(password_hash)
+	  end
+
+	  def password=(new_password)
+	    @password = Password.create(new_password)
+	    self.encrypted_password = @password
+	  end
+
+	# def login
+	#   @user = User.find_by_email(params[:email])
+	#   if @user.password == params[:password]
+	#     give_token
+	#   else
+	#     redirect_to home_url
+	#   end
 	
- def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
-end
-
-def create
-  @user = User.new(params[:user])
-  @user.password = params[:password]
-  @user.save!
-end
-
-def login
-  @user = User.find_by_email(params[:email])
-  if @user.password == params[:password]
-    give_token
-  else
-    redirect_to home_url
-  end
-end
-
 end
